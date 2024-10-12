218adfebc50dd78f27e5e4e636cec9510aceb966
kabir4874 <kabir.cse.bd@gmail.com>
2024-10-12 19:07:37 +0600

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { CompanyModule } from './company/company.module';
+import { JobModule } from './job/job.module';
+import { ApplicationModule } from './application/application.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, UserModule, CompanyModule, JobModule, ApplicationModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
