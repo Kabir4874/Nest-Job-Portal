9386e3eb383d162e5ff3b298a9bdfdafaace707e
kabir4874 <kabir.cse.bd@gmail.com>
2024-10-14 17:00:01 +0600

    update

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CompanyService } from './company.service';
+import { RegisterCompanyDto } from './dto/company.dto';
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+  constructor(private readonly companyService: CompanyService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('register')
+  async registerCompany(
+    @Req() req: Request,
+    @Body() registerCompanyDto: RegisterCompanyDto,
+  ) {
+    const userId = req.user.id;
+    const result = await this.companyService.registerCompany(
+      userId,
+      registerCompanyDto,
+    );
+  }
+}
